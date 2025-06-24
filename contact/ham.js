6fed6fcc4ae36c377be3336f39e3022c8d24404c
kyokasuigetsu <kyoukasuigetsu.809@gmail.com>
2025-06-25 08:52:50 +0900

    0625

--- contact/ham.js
+++ contact/ham.js
@@ -4,3 +4,17 @@ $(function(){
         $(this).toggleClass('active');
     });
 });
+var validateText = function(e) {
+        var validated = '';
+        Array.prototype.forEach.call(e.value, function(c) {
+          if (
+            c.match(/[A-Za-z0-9]/)
+            || c.match(/[\n|\r\n|\r]/)
+            || c.match(/[\u30a0-\u30ff\u3040-\u309f]/)
+            || c.match(/[ｦ-ﾟ]/)
+          ) {
+            validated += c;
+          }
+        });
+        e.value = validated;
+      }
